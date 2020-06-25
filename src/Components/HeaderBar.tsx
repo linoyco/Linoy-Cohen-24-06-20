0a9d1b20b9885a522a28e5c067eec5610f0eaf40
Linoy Cohen <45672703+linoyco@users.noreply.github.com>
2020-06-25 15:47:32 +0300

    styled headerBar

--- src/Components/HeaderBar.tsx
+++ src/Components/HeaderBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Brightness4, Favorite, Home } from '@material-ui/icons';
 
 const StyledDiv: any = styled.div`
     background-color: #4E5D89;
@@ -9,14 +10,34 @@ const StyledDiv: any = styled.div`
     justify-content: center;
     align-items: center;
     align-content: center;
+`;
 
-    padding: 1%;
+const StyledTitle: any = styled.h1`
+    margin-left: 1%;
+    margin-right: auto;
+`;
+
+const StyledIconName: any = styled.p`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 8px;
+`;
+
+const StyledDivIcons: any = styled.div`
+    display: flex;
+    margin-right: 1%;
 `;
 
 const HeaderBar: React.FunctionComponent = () => (
-    <div>
-        <StyledDiv>HeaderBar</StyledDiv>
-    </div>
+    <StyledDiv>
+        <StyledTitle>National Current Weather</StyledTitle>
+        <StyledDivIcons>
+            <StyledIconName><Brightness4 />Mode</StyledIconName>
+            <StyledIconName><Favorite />Favorites</StyledIconName>
+            <StyledIconName><Home />Home</StyledIconName>
+        </StyledDivIcons>
+    </StyledDiv>
 );
 
 export default HeaderBar;
