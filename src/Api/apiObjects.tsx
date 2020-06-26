0da39e638596295a03ebd5188735fce15d41a1df
Linoy Cohen <45672703+linoyco@users.noreply.github.com>
2020-06-26 15:23:08 +0300

    api objects

---
+++ src/Api/apiObjects.tsx
@@ -0,0 +1,144 @@
+export interface IAutocompleteOBJ {
+    Version: number,
+    Key: string,
+    Type: string,
+    Rank: number,
+    LocalizedName: string,
+    Country: {
+        ID: string,
+        LocalizedName: string
+    },
+    AdministrativeArea: {
+        ID: string,
+        LocalizedName: string
+    }
+};
+
+export interface ICurrentWeatherOBJ {
+    LocalObservationDateTime: string,
+    EpochTime: number,
+    WeatherText: string,
+    WeatherIcon: number,
+    HasPrecipitation: boolean,
+    PrecipitationType: any,
+    IsDayTime: boolean,
+    Temperature: {
+        Metric: {
+            Value: number,
+            Unit: string,
+            UnitType: number
+        },
+        Imperial: {
+            Value: number,
+            Unit: string,
+            UnitType: number
+        }
+    },
+    MobileLink: string,
+    Link: string
+};
+
+interface IDailyForecastsOBJ {
+    Date: string,
+    EpochDate: number,
+    Temperature: {
+        Minimum: {
+            Value: number,
+            Unit: string,
+            UnitType: number
+        },
+        Maximum: {
+            Value: number,
+            Unit: string,
+            UnitType: number
+        }
+    },
+    Day: {
+        Icon: number,
+        IconPhrase: string,
+        HasPrecipitation: boolean
+    },
+    Night: {
+        Icon: number,
+        IconPhrase: string,
+        HasPrecipitation: boolean
+    },
+    Sources: string[],
+    MobileLink: string,
+    Link: string
+};
+
+interface IHeadlineOBJ {
+    EffectiveDate: string,
+    EffectiveEpochDate: number,
+    Severity: number,
+    Text: string,
+    Category: string,
+    EndDate: any,
+    EndEpochDate: any,
+    MobileLink: string,
+    Link: string
+};
+
+export interface IFiveDaysWeatherOBJ {
+    Headline: IHeadlineOBJ,
+    DailyForecasts: Array<IDailyForecastsOBJ>
+};
+
+export interface IGeolocationKeyOBJ {
+    Version: number,
+    Key: string,
+    Type: string,
+    Rank: number,
+    LocalizedName: string,
+    EnglishName: string,
+    PrimaryPostalCode: string,
+    Region: {
+        ID: string,
+        LocalizedName: string,
+        EnglishName: string
+    },
+    Country: {
+        ID: string,
+        LocalizedName: string,
+        EnglishName: string
+    },
+    AdministrativeArea: {
+        ID: string,
+        LocalizedName: string,
+        EnglishName: string,
+        Level: number,
+        LocalizedType: string,
+        EnglishType: string,
+        CountryID: string
+    },
+    TimeZone: {
+        Code: string,
+        Name: string,
+        GmtOffset: number,
+        IsDaylightSaving: boolean,
+        NextOffsetChange: string
+    },
+    GeoPosition: {
+        Latitude: number,
+        Longitude: number,
+        Elevation: {
+            Metric: {
+                Value: number,
+                Unit: string,
+                UnitType: number
+            },
+            Imperial: {
+                Value: number,
+                Unit: string,
+                UnitType: number
+            }
+        }
+    },
+    IsAlias: boolean,
+    SupplementalAdminAreas: [],
+    DataSets: string[]
+};
+
+
+
--- src/State/Reducers/app.ts
+++ src/State/Reducers/app.ts
@@ -1,12 +1,86 @@
 import produce from 'immer';
 import { SET_LOCATION, appActionType } from '../Actions/App/types';
+import { IAutocompleteOBJ, ICurrentWeatherOBJ, IFiveDaysWeatherOBJ, IGeolocationKeyOBJ } from '../../Api/apiObjects';
 
 export interface IAppState {
-    location: string;
+    location: string,
+    autocompleteList: Array<IAutocompleteOBJ>,
+    currentWeatherList: Array<ICurrentWeatherOBJ>,
+    fiveDaysWeather: IFiveDaysWeatherOBJ,
+    geolocationKey: IGeolocationKeyOBJ
 }
 
 const initialState: IAppState = {
-    location: 'Tel-Aviv'
+    location: 'Tel-Aviv',
+    autocompleteList: [],
+    currentWeatherList: [],
+    fiveDaysWeather: {
+        Headline: {
+            EffectiveDate: '',
+            EffectiveEpochDate: 0,
+            Severity: 0,
+            Text: '',
+            Category: '',
+            EndDate: null,
+            EndEpochDate: null,
+            MobileLink: '',
+            Link: ''
+        }, DailyForecasts: []
+    },
+    geolocationKey: {
+        Version: 0,
+        Key: '',
+        Type: '',
+        Rank: 0,
+        LocalizedName: '',
+        EnglishName: '',
+        PrimaryPostalCode: '',
+        Region: {
+            ID: '',
+            LocalizedName: '',
+            EnglishName: ''
+        },
+        Country: {
+            ID: '',
+            LocalizedName: '',
+            EnglishName: ''
+        },
+        AdministrativeArea: {
+            ID: '',
+            LocalizedName: '',
+            EnglishName: '',
+            Level: 0,
+            LocalizedType: '',
+            EnglishType: '',
+            CountryID: ''
+        },
+        TimeZone: {
+            Code: '',
+            Name: '',
+            GmtOffset: 0,
+            IsDaylightSaving: false,
+            NextOffsetChange: ''
+        },
+        GeoPosition: {
+            Latitude: 0,
+            Longitude: 0,
+            Elevation: {
+                Metric: {
+                    Value: 0,
+                    Unit: '',
+                    UnitType: 0
+                },
+                Imperial: {
+                    Value: 0,
+                    Unit: '',
+                    UnitType: 0
+                }
+            }
+        },
+        IsAlias: false,
+        SupplementalAdminAreas: [],
+        DataSets: []
+    }
 }
 
 export function appReducer(state: IAppState = initialState, action: appActionType) {
